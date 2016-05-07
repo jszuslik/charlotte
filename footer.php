6e91443fd8e5455579d3462f0a214e9e0ec4ebdb
Joshua Szuslik <joshuaszuslik@gmail.com>
2016-05-07 12:23:01 -0500

    init

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoshuaSzuslik
+ * Date: 5/1/16
+ * Time: 12:35 AM
+ */?>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoshuaSzuslik
+ * Date: 5/1/16
+ * Time: 12:33 AM
+ */
+get_header();?>
+
+<?php get_footer(); ?>
